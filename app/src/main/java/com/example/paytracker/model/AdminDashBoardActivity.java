ebe86fda032cc2b2fc710e3843cb9eb9c5288d72
nehajohn <nehajohn1022@gmail.com>
2020-09-30 12:36:27 -0400

    AdminDashBoardActivityAdded

---
+++ app/src/main/java/com/example/paytracker/model/AdminDashBoardActivity.java
@@ -0,0 +1,65 @@
+package com.example.paytracker.model;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class AdminDashBoardActivity extends AppCompatActivity {
+    CardView cd_add_provinces,cd_view_provinces,cd_view_texas,cd_add_texas;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Admin Dashboard");
+
+        cd_add_provinces=(CardView)findViewById(R.id.cd_add_provinces);
+        cd_view_provinces=(CardView)findViewById(R.id.cd_view_provinces);
+        cd_view_texas=(CardView)findViewById(R.id.cd_view_texas);
+        cd_add_texas=(CardView)findViewById(R.id.cd_add_texas);
+
+        cd_add_provinces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminDashBoardActivity.this,AddProvincesActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        cd_view_provinces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(AdminDashBoardActivity.this,ViewProvincesActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        cd_add_texas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminDashBoardActivity.this,AddTaxesActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        cd_view_texas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminDashBoardActivity.this,ViewTaxesActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
