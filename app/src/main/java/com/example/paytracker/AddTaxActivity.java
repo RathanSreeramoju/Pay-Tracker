1377b650c7d13f102184895d8d33cda499674439
ipraneeth7 <inupamulapraneeth@gmail.com>
2020-09-30 23:35:49 -0400

    Created admin add tax page Ui

---
+++ app/src/main/java/com/example/paytracker/AddTaxActivity.java
@@ -0,0 +1,14 @@
+package com.example.paytracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTaxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tax);
+    }
+}
