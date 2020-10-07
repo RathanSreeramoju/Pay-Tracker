088881f54929ce2e515b7d8ee820b264d5b2183c
Manoj496 <manoj.legendary@gmail.com>
2020-10-07 19:28:47 -0400

    Designed Settings Page Ui and added Java code

--- app/src/main/java/com/example/paytracker/SettingsActivity.java
+++ app/src/main/java/com/example/paytracker/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.paytracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override                                                                                                                    //add this method in your program
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
