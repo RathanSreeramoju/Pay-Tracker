c85f4f395c7d64cf1baa12cb17afa118db2c790c
ipraneeth7 <inupamulapraneeth@gmail.com>
2020-10-19 18:14:24 -0400

    added  functionality in MyWorksActivity

---
+++ app/src/main/java/com/example/paytracker/GetMyWorksAdapter.java
@@ -0,0 +1,12 @@
+package com.example.paytracker;
+
+import android.widget.ListAdapter;
+
+import com.example.paytracker.model.PaymentPojo;
+
+import java.util.List;
+
+public class GetMyWorksAdapter implements ListAdapter {
+    public GetMyWorksAdapter(List<PaymentPojo> a1, MyWorksActivity myWorksActivity) {
+    }
+}
