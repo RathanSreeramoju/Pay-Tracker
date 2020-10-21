1175c70fb6db835c8c3b25f1c0b2bc807d387d78
ipraneeth7 <inupamulapraneeth@gmail.com>
2020-10-20 22:18:03 -0400

    changes in MyWorksActivity

--- app/src/main/java/com/example/paytracker/MyWorksActivity.java
+++ app/src/main/java/com/example/paytracker/MyWorksActivity.java
@@ -6,13 +6,18 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.paytracker.adapter.GetMyWorksAdapter;
 import com.example.paytracker.api.ApiService;
 import com.example.paytracker.api.RetroClient;
+import com.example.paytracker.model.JobTitlePojo;
 import com.example.paytracker.model.PaymentPojo;
 
 import java.util.ArrayList;
@@ -75,6 +80,66 @@ public class MyWorksActivity extends AppCompatActivity {
             public void onFailure(Call<List<PaymentPojo>> call, Throwable t) {
                 progressDialog.dismiss();
                 Toast.makeText(MyWorksActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
+    List<JobTitlePojo> array_jobs;
+    String myjobs[];
+    private void getJobs() {
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<List<JobTitlePojo>> call = apiService.getjobtypes_by_uname(session);
+        call.enqueue(new Callback<List<JobTitlePojo>>() {
+            @Override
+            public void onResponse(Call<List<JobTitlePojo>> call, Response<List<JobTitlePojo>> response) {
+                array_jobs = response.body();
+                if(array_jobs!=null){
+                    if(array_jobs.size()>0) {
+                        myjobs = new String[array_jobs.size()+1];
+                        myjobs[0] = "Select Job";
+                        Toast.makeText(getApplicationContext(),""+array_jobs.size(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                for (int i = 0; i < array_jobs.size(); i++) {
+                    myjobs[i + 1] = array_jobs.get(i).getCompany_name();
+                }
+                ArrayAdapter aa = new ArrayAdapter(MyWorksActivity.this, android.R.layout.simple_spinner_item, myjobs);
+                aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                spin_jobs.setAdapter(aa);
+
+                spin_jobs.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                        if (pos > 0) {
+                            getMyWorksAdapter.filter(spin_jobs.getSelectedItem().toString());
+
+
+
+                        }
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<List<JobTitlePojo>> call, Throwable t) {
+                Log.d("TAG", "Response = " + t.toString());
             }
         });
     }
