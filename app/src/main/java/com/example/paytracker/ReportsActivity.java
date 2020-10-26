aa0ae5d39848b79670d8e64ad78497599aa7bc66
MounikaChowdary1113 <mounicamouni1113@gmail.com>
2020-10-26 11:04:21 -0400

    ReportsActivity code updated.

--- app/src/main/java/com/example/paytracker/ReportsActivity.java
+++ app/src/main/java/com/example/paytracker/ReportsActivity.java
@@ -6,13 +6,19 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.paytracker.adapter.ReportsAdapter;
 import com.example.paytracker.api.ApiService;
 import com.example.paytracker.api.RetroClient;
+import com.example.paytracker.model.JobTitlePojo;
 import com.example.paytracker.model.PaymentPojo;
 
 import java.util.List;
@@ -23,6 +29,10 @@ import retrofit2.Response;
 
 public class ReportsActivity extends BaseActivity {
     ListView lv;
+    Spinner spin_jobs;
+    SharedPreferences sharedPreferences;
+    String session;
+    ReportsAdapter reportsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +40,13 @@ public class ReportsActivity extends BaseActivity {
         getSupportActionBar().setTitle(" Reports");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        session = sharedPreferences.getString("uname", "def-val");
+
+        spin_jobs=(Spinner)findViewById(R.id.spin_jobs);
+        getJobs();
+
         lv=(ListView)findViewById(R.id.lv);
         serverData();
         Toast.makeText(getApplicationContext(),getIntent().getStringExtra("start_date")+"  "+getIntent().getStringExtra("end_date"),Toast.LENGTH_SHORT).show();
@@ -47,20 +64,70 @@ public class ReportsActivity extends BaseActivity {
         call.enqueue(new Callback<List<PaymentPojo>>() {
             @Override
             public void onResponse(Call<List<PaymentPojo>> call, Response<List<PaymentPojo>> response) {
-                //Toast.makeText(AvalableRoomsActivity.this,"ddddd   "+response.body().size(),Toast.LENGTH_SHORT).show();
+
                 progressDialog.dismiss();
                 if(response.body()==null){
                     Toast.makeText(ReportsActivity.this,"No data found",Toast.LENGTH_SHORT).show();
                 }else {
                     payments = response.body();
                     //Toast.makeText(ReportsActivity.this,""+payments.size(),Toast.LENGTH_SHORT).show();
-                    lv.setAdapter(new ReportsAdapter(payments, ReportsActivity.this));
+                    reportsAdapter=new ReportsAdapter(payments, ReportsActivity.this);
+                    lv.setAdapter(reportsAdapter);
+                   // lv.setAdapter(new ReportsAdapter(payments, ReportsActivity.this));
                 }
             }
             @Override
             public void onFailure(Call<List<PaymentPojo>> call, Throwable t) {
                 progressDialog.dismiss();
                 Toast.makeText(ReportsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    List<JobTitlePojo> array_jobs;
+    String myjobs[];
+    private void getJobs() {
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<List<JobTitlePojo>> call = apiService.getjobtypes_by_uname(session);
+        call.enqueue(new Callback<List<JobTitlePojo>>() {
+            @Override
+            public void onResponse(Call<List<JobTitlePojo>> call, Response<List<JobTitlePojo>> response) {
+                array_jobs = response.body();
+                if(array_jobs!=null){
+                    if(array_jobs.size()>0) {
+                        myjobs = new String[array_jobs.size()+1];
+                        myjobs[0] = "Select Job";
+                        Toast.makeText(getApplicationContext(),""+array_jobs.size(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                for (int i = 0; i < array_jobs.size(); i++) {
+                    myjobs[i + 1] = array_jobs.get(i).getCompany_name();
+                }
+                ArrayAdapter aa = new ArrayAdapter(ReportsActivity.this, android.R.layout.simple_spinner_item, myjobs);
+                aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spin_jobs.setAdapter(aa);
+
+                spin_jobs.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                        if (pos > 0) {
+                            reportsAdapter.filter(spin_jobs.getSelectedItem().toString());
+
+
+
+                        }
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<List<JobTitlePojo>> call, Throwable t) {
+                Log.d("TAG", "Response = " + t.toString());
             }
         });
     }
--- app/src/main/java/com/example/paytracker/adapter/ReportsAdapter.java
+++ app/src/main/java/com/example/paytracker/adapter/ReportsAdapter.java
@@ -1,30 +1,38 @@
 package com.example.paytracker.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import com.example.paytracker.PaySlipActivity;
 import com.example.paytracker.R;
 import com.example.paytracker.model.PaymentPojo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ReportsAdapter extends BaseAdapter {
 
-    List<PaymentPojo> ar;
+    List<PaymentPojo> paymentPojo,searchbyjob;
     Context cnt;
 
     public ReportsAdapter(List<PaymentPojo> ar, Context cnt) {
-        this.ar = ar;
+        this.searchbyjob=ar;
         this.cnt = cnt;
+        this.paymentPojo = new ArrayList<PaymentPojo>();
+        this.paymentPojo.addAll(ar);
     }
-
     @Override
     public int getCount() {
-        return ar.size();
+        return paymentPojo.size();
     }
+
 
     @Override
     public Object getItem(int i) {
@@ -43,20 +51,61 @@ public class ReportsAdapter extends BaseAdapter {
 
 
         TextView tv_work_date = (TextView) obj2.findViewById(R.id.tv_work_date);
-        tv_work_date.setText("Date : " + ar.get(pos).getWork_date());
+        tv_work_date.setText(paymentPojo.get(pos).getWork_date());
 
         TextView tv_start_time = (TextView) obj2.findViewById(R.id.tv_start_time);
-        tv_start_time.setText("Start Date : " + ar.get(pos).getStart_time());
+        tv_start_time.setText(paymentPojo.get(pos).getStart_time());
 
         TextView tv_end_time = (TextView) obj2.findViewById(R.id.tv_end_time);
-        tv_end_time.setText("End Date : " + ar.get(pos).getEnd_time());
+        tv_end_time.setText(paymentPojo.get(pos).getEnd_time());
 
         TextView tv_payment = (TextView) obj2.findViewById(R.id.tv_payment);
-        tv_payment.setText("Payment : " + ar.get(pos).getEnd_time());
+        tv_payment.setText(paymentPojo.get(pos).getPayment());
 
-        TextView tv_payment_deduct = (TextView) obj2.findViewById(R.id.tv_payment_deduct);
-        tv_payment_deduct.setText("Tax Deduct: " + ar.get(pos).getPayment_deduct());
+        TextView tv_company_name = (TextView) obj2.findViewById(R.id.tv_company_name);
+        tv_company_name.setText(paymentPojo.get(pos).getCompany_name());
+
+        TableRow tableRow1=(TableRow)obj2.findViewById(R.id.tableRow1);
+        tableRow1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(cnt, PaySlipActivity.class);
+                intent.putExtra("date",paymentPojo.get(pos).getWork_date());
+                intent.putExtra("cname",paymentPojo.get(pos).getCompany_name());
+                intent.putExtra("stime",paymentPojo.get(pos).getStart_time());
+                intent.putExtra("etime",paymentPojo.get(pos).getEnd_time());
+                intent.putExtra("thours",paymentPojo.get(pos).getTotal_hours());
+                intent.putExtra("breaks",paymentPojo.get(pos).getBreak_time());
+                intent.putExtra("sal",paymentPojo.get(pos).getSal_hour());
+                intent.putExtra("tax",paymentPojo.get(pos).getTax());
+                intent.putExtra("dtax",paymentPojo.get(pos).getPayment_deduct());
+                intent.putExtra("net",paymentPojo.get(pos).getPayment());
+                cnt.startActivity(intent);
+            }
+        });
+
+
 
         return obj2;
     }
+    public void filter(String charText)
+    {
+        charText = charText.toLowerCase(Locale.getDefault());
+        paymentPojo.clear();
+        if (charText.length() == 0) {
+            paymentPojo.addAll(searchbyjob);
+        } else {
+            for (PaymentPojo wp : searchbyjob) {
+                if (wp.getCompany_name().toLowerCase(Locale.getDefault()).contains(charText) ) {
+                    paymentPojo.add(wp);
+                }
+                /*else{
+                    Toast.makeText(cnt, "There is no data related to selected Companyname", Toast.LENGTH_SHORT).show();
+                }*/
+            }
+        }
+        notifyDataSetChanged();
+    }
 }
+
+
