be4cf4c25e96c403a80510f9269e4d64bc65ed3d
tinkuaravind <aravind.tinku257@gmail.com>
2020-10-27 18:07:22 -0400

    Added statistics functionality in userdashboard

--- app/src/main/java/com/example/paytracker/UserDashboardActivity.java
+++ app/src/main/java/com/example/paytracker/UserDashboardActivity.java
@@ -1,13 +1,6 @@
 package com.example.paytracker;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,28 +9,70 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.navigation.NavigationView;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserDashboardActivity extends AppCompatActivity {
     private ActionBarDrawerToggle t;
     private NavigationView nv;
     private DrawerLayout dl;
+    BarChart barChart;
+    BarData barData,data;
+    BarDataSet barDataSet;
+    ArrayList barEntries;
+    PieChart p;
+    ArrayList<String> labels;
+    ArrayList<String> dates=new ArrayList<String>();
+    int t1=0;
+    ArrayList<BarEntry> entries = new ArrayList<>();
+    ArrayList<Entry> entries1 = new ArrayList<>();
 
     int day,month,year;
     ImageView image_add;
     TextView tv_add_work,addjob;
     CardView cd_daily,cd_weekly,cd_monthly,cd_yearly;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
         navigationView();
         getSupportActionBar().setTitle("User Dashboard");
+        barChart=findViewById(R.id.barchart);
+        p=findViewById(R.id.piechart);
+        getdata();
+
 
         tv_add_work=(TextView)findViewById(R.id.tv_add_work);
 
@@ -50,14 +85,7 @@ public class UserDashboardActivity extends AppCompatActivity {
             }
         });
 
-        addjob=(TextView)findViewById(R.id.addjob);
-        addjob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(UserDashboardActivity.this,GetAllJobProfileActivity.class);
-                startActivity(intent);
-            }
-        });
+
 
         image_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -161,6 +189,89 @@ public class UserDashboardActivity extends AppCompatActivity {
             }
         });
     }
+    private void getdata() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url ="http://paytracker.ca/PayTracker/getmonthreport.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,url , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                JSONObject json = null;
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    // data=new String[jArray.length()];
+                    JSONObject jObj;
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        jObj = jsonArray.getJSONObject(i);
+                        String data=jObj.getString("work_date");
+                        String link=jObj.getString("payment");
+                        Toast.makeText(UserDashboardActivity.this,data+""+link,Toast.LENGTH_LONG).show();
+                        entries.add(new BarEntry(Integer.parseInt(link), t1++));
+                        entries1.add(new Entry(Integer.parseInt(link), t1));
+                        dates.add(data);
+                    }
+
+
+                    BarDataSet bardataset = new BarDataSet(entries, "Lastest Week Report");
+
+
+
+                    BarData data = new BarData( dates, bardataset);
+                    barChart.setData(data); // set the data and list of labels into chart
+                    // set the description
+                    bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+                    barChart.animateY(5000);
+
+
+
+
+
+                    PieDataSet pd = new PieDataSet(entries1, "Lastest Week Report");
+
+
+
+                    PieData pp = new PieData( dates, pd);
+                    p.setData(pp); // set the data and list of labels into chart
+                    // set the description
+                    pd.setColors(ColorTemplate.COLORFUL_COLORS);
+                    p.setDescription("Pie Chart");
+                    p.setCenterText("Clock Work");
+                    //p.animateY(5000);
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"Server Issue",Toast.LENGTH_SHORT).show();
+            }
+        })
+
+        {
+            @Override
+            protected Map<String, String> getParams() {
+
+                sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+                session = sharedPreferences.getString("uname", "def-val");
+                Map<String, String> params = new HashMap<String, String>();
+                //  params.put("type",sptype.getSelectedItem().toString());//
+                params.put("email",session);//
+
+
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+
+
+    }
+
     private void navigationView(){
         dl = (DrawerLayout)findViewById(R.id.dl_main);
         t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
