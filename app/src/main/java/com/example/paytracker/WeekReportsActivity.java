243f1adbbf65a1fd254d4a84645f4faed1d23086
MounikaChowdary1113 <mounicamouni1113@gmail.com>
2020-10-30 00:53:13 -0400

    Week Reports java code activity is updated

--- app/src/main/java/com/example/paytracker/WeekReportsActivity.java
+++ app/src/main/java/com/example/paytracker/WeekReportsActivity.java
@@ -2,13 +2,126 @@ package com.example.paytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import com.example.paytracker.adapter.ReportsAdapter;
+import com.example.paytracker.api.ApiService;
+import com.example.paytracker.api.RetroClient;
+import com.example.paytracker.model.JobTitlePojo;
+import com.example.paytracker.model.PaymentPojo;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WeekReportsActivity extends AppCompatActivity {
 
+    ListView lv;
+    Spinner spin_jobs;
+    SharedPreferences sharedPreferences;
+    String session;
+
+    BarChart barChart;
+    BarData barData,data;
+    BarDataSet barDataSet;
+    ArrayList barEntries;
+    PieChart p;
+    ArrayList<String> labels;
+    ArrayList<String> dates=new ArrayList<String>();
+    int t1=0;
+    ArrayList<BarEntry> entries = new ArrayList<>();
+    ArrayList<Entry> entries1 = new ArrayList<>();
+    ReportsAdapter reportsAdapters;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week_reports);
+        getSupportActionBar().setTitle("Weekly Reports");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        barChart=findViewById(R.id.barchart);
+        p=findViewById(R.id.piechart);
+        getdata();
+
+        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        session = sharedPreferences.getString("uname", "def-val");
+
+        spin_jobs=(Spinner)findViewById(R.id.spin_jobs);
+        getJobs();
+
+        lv=(ListView)findViewById(R.id.lv);
+        serverData();
+        Toast.makeText(getApplicationContext(),getIntent().getStringExtra("start_date")+"  "+getIntent().getStringExtra("end_date"),Toast.LENGTH_SHORT).show();
+
+
     }
+    ProgressDialog progressDialog;
+    List<PaymentPojo> payments;
+    private void serverData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        progressDialog = new ProgressDialog(WeekReportsActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<List<PaymentPojo>> call = service.get_reports(sharedPreferences.getString("uname","-"),getIntent().getStringExtra("end_date"),getIntent().getStringExtra("start_date"));
+        call.enqueue(new Callback<List<PaymentPojo>>() {
+            @Override
+            public void onResponse(Call<List<PaymentPojo>> call, Response<List<PaymentPojo>> response) {
+                //Toast.makeText(AvalableRoomsActivity.this,"ddddd   "+response.body().size(),Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+                if(response.body()==null){
+                    Toast.makeText(WeekReportsActivity.this,"No data found",Toast.LENGTH_SHORT).show();
+                }else {
+                    payments = response.body();
+                    //Toast.makeText(ReportsActivity.this,""+payments.size(),Toast.LENGTH_SHORT).show();
+
+                    reportsAdapters=new ReportsAdapter(payments, WeekReportsActivity.this);
+                    lv.setAdapter(reportsAdapters);
+                    //lv.setAdapter(new ReportsAdapters(payments, ReportsActivity.this));
+                }
+            }
+            @Override
+            public void onFailure(Call<List<PaymentPojo>> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(WeekReportsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    List<JobTitlePojo> array_jobs;
+    String myjobs[];
+private void getdata(){
+
+}
+private void getJobs(){
+
+}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
